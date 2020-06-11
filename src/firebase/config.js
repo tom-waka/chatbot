73c2a6196a16dce7fcdf74ddc283864c8cd1f023
tom-waka <one.sukt.one@gmail.com>
2020-06-11 23:21:19 +0900

    firestoreからデータを取得するように変更

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByXIPyniGQMrdtNIQuBLFojttx69-J0yo",
+  authDomain: "chatbot-a8004.firebaseapp.com",
+  databaseURL: "https://chatbot-a8004.firebaseio.com",
+  projectId: "chatbot-a8004",
+  storageBucket: "chatbot-a8004.appspot.com",
+  messagingSenderId: "630140338094",
+  appId: "1:630140338094:web:d0ebcc21b7a15aa251523f",
+  measurementId: "G-ZK5YT360L8"
+};
+
+export default firebaseConfig;
